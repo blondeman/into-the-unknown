c0c42dc1b584ed266f98d08ac4c5d42906bcacba
Dominik David <domo23000@gmail.com>
2026-04-26 20:55:53 -0600

    random death sounds

--- scripts/OneShot.gd
+++ scripts/OneShot.gd
@@ -1,8 +1,12 @@
 extends CPUParticles3D
 
 @export var timer: float = 2
+@export var sounds: Array[AudioStream]
+@export var audio_stream_player: AudioStreamPlayer3D
 
 func start() -> void:
 	emitting = true
+	audio_stream_player.stream = sounds[randi() % sounds.size()]
+	audio_stream_player.play()
 	await get_tree().create_timer(timer).timeout
 	queue_free()
