41e92337a32c6866964e6dfe69532862bfd97d83
Graham Crosbie <grahamjcros@gmail.com>
2026-05-03 07:11:57 -0600

    hacked

--- scripts/enemy/AttackMelee.gd
+++ scripts/enemy/AttackMelee.gd
@@ -1,6 +1,12 @@
 @tool
 extends Attack
 
+@onready var anim_player = $"../Mesh/AnimationPlayer"
+
+func _ready():
+	print(anim_player.get_animation_list())
 
 func _attack(target: Node3D):
+	if anim_player.current_animation != "RedTeam_SwordsMen_Armature|Atack_SwordsMen":
+		anim_player.play("RedTeam_SwordsMen_Armature|Atack_SwordsMen")
 	deal_damage(target)
--- scripts/enemy/EnemyController.gd
+++ scripts/enemy/EnemyController.gd
@@ -3,14 +3,12 @@ extends CharacterBody3D
 
 @export var speed = 5.0
 @export var target: Node3D
-@onready var mesh: Node3D = $RedTeam_Archer_Unit
-@onready var anim_player: AnimationPlayer
+@onready var mesh: Node3D = $Mesh
 var direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	if !target:
 		target = get_tree().get_first_node_in_group("player")
-	anim_player = mesh.get_node("AnimationPlayer")
 
 func set_direction(_direction: Vector2):
 	direction = _direction
@@ -28,7 +26,6 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
-	_update_animation()
 
 func _rotate_mesh(delta: float) -> void:
 	if not mesh:
@@ -39,18 +36,3 @@ func _rotate_mesh(delta: float) -> void:
 		return
 	var target_basis := Basis.looking_at(-move_dir.normalized(), Vector3.UP)
 	mesh.global_basis = mesh.global_basis.slerp(target_basis, delta * 10.0)
-	
-func _update_animation() -> void:
-	if not anim_player:
-		return
-
-	var moving := direction.length_squared() > 0.001
-
-	if moving:
-		_play_anim("Running_Archer") 
-	else:
-		_play_anim("idle")
-
-func _play_anim(anim_name: String) -> void:
-	if anim_player.current_animation != anim_name:
-		anim_player.play(anim_name)
